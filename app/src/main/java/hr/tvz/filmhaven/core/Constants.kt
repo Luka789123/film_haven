c3dd30145c83704c114c9a29309100c6c3341751
Luka789123 <96635976+Luka789123@users.noreply.github.com>
2025-02-17 23:43:39 +0100

    Delete app/src/main/java/hr/tvz/filmhaven/core/Constants.kt

--- app/src/main/java/hr/tvz/filmhaven/core/Constants.kt
+++
@@ -1,7 +0,0 @@
-package hr.tvz.filmhaven.core
-
-object Constants {
-    val BASE_URL:String = "https://api.themoviedb.org/3/";
-    const val AUTHENTICATION_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZWRmZDJhMDU0MDk3Mjk2OThiMzYyOTI0YTc2NTdlOSIsIm5iZiI6MTcyNjI0ODY4Mi4wOCwic3ViIjoiNjZlNDc2ZWEwMDAwMDAwMDAwYjljZjIyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.WoVTHtt2MG4JTDNiAkS4Sf7wzRG54gc5QWvfmGT44Og"
-    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
-}
